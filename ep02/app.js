c558b3a15bc5980107cab84f7b6366be6cdaeaf8
Anuj Chaturvedi <67587859+a-tenet-web-developer@users.noreply.github.com>
2024-10-10 16:57:18 +0530

    course material for ep02

---
+++ ep02/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+const heading = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", {}, [
+    React.createElement("h2", { id: "heading" }, "this is an another heading"),
+    React.createElement("h2", { id: "headin" }, "this is an another heading"),
+  ])
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
